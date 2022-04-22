57126a021ea27597ce6aa30c41624c6bde9f9d38
Eddie Garcia <edd1e234g@gmail.com>
2022-04-22 17:48:13 -0600

    Approach 1 working

---
+++ main.py
@@ -0,0 +1,11 @@
+from scripts.get_data import get_data, get_data_2,create_date_sample_data_set_from_org, get_lifetime_prices_every_hour, get_lifetime_prices_once_per_day
+
+#get_data()
+
+#create_date_sample_data_set_from_org()
+
+#get_data_2()
+
+#get_lifetime_prices_every_hour()
+
+get_lifetime_prices_once_per_day()
